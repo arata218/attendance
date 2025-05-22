b25a23aaf8558bfa862366698144f67b9456fff8
arata <arata218@icloud.com>
2025-05-22 13:16:07 +0900

    refactor: update Calendar component to use FullCalendar and remove unused routes and components

--- islands/Calendar.tsx
+++ islands/Calendar.tsx
@@ -1,62 +1,32 @@
-import { useEffect } from "preact/hooks";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { useEffect, useRef } from "preact/hooks";
 
-export default function Calendar() {
+export default function CalendarComponent() {
+  const calendarRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (!IS_BROWSER) return;
+    if (!calendarRef.current) return;
 
-    // スタイルシートの追加
-    // const link = document.createElement("link");
-    // link.href = "https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/main.min.css";
-    // link.rel = "stylesheet";
-    // document.head.appendChild(link);
+    const calendar = new Calendar(calendarRef.current, {
+      plugins: [dayGridPlugin],
+      initialView: "dayGridMonth",
+      headerToolbar: {
+        left: "prev,next",
+        center: "title",
+        right: "today",
+      },
+      locale: "ja",
+    });
+    calendar.render();
 
-    // FullCalendarの読み込み
-    const script = document.createElement("script");
-    script.src =
-      "https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.js";
-    script.async = true;
-    script.onload = () => {
-      const calendarEl = document.getElementById("calendar");
-      if (!calendarEl) return;
-
-      const calendar = new (window as any).FullCalendar.Calendar(calendarEl, {
-        initialView: "dayGridMonth",
-        headerToolbar: {
-          left: "prev,next",
-          center: "title",
-          right: "today",
-          // right: "dayGridMonth,timeGridWeek,timeGridDay",
-        },
-        locale: "ja",
-        // events: [
-        //   { title: "山田：出席", date: "2024-03-14", color: "green" },
-        //   { title: "佐藤：欠席", date: "2024-03-14", color: "red" },
-        // ],
-        // dateClick: function (info: { dateStr: string }) {
-        //   alert("選択された日付: " + info.dateStr);
-        // },
-      });
-      calendar.render();
-    };
-    script.onerror = (error) => {
-      console.error("カレンダーの読み込みに失敗しました:", error);
-    };
-    document.head.appendChild(script);
-
-    return () => {
-      // クリーンアップ
-      // document.head.removeChild(link);
-      document.head.removeChild(script);
-    };
+    return () => calendar.destroy();
   }, []);
 
   return (
-    <div class="p-4">
-      <div
-        id="calendar"
-        class="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-4"
-      />
-    </div>
+    <div
+      ref={calendarRef}
+      class="bg-white shadow-lg mx-auto p-6 rounded-lg max-w-4xl"
+    />
   );
 }
